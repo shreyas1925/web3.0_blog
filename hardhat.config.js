b8dd0d04c37de8954ac5d4f755052db0039f2bc6
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-04-14 23:58:24 +0530

    basic features completed with bugs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: "0.8.4",
   // some network configurations
   networks: {
     hardhat: {
-      chainId: 1337 //local host of metmask have this chainID
+      chainId: 1337
     },
     // mumbai: {
     //   url: "https://rpc-mumbai.matic.today",
     //   accounts: [process.env.pk]
     // },
-    // polygon: {
-    //   url: "https://polygon-rpc.com/",
-    //   accounts: [process.env.pk]
-    // }
+    polygon: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.pk]
+    }
   }
 };

034c72cdcc4f6d7282b4fa59f7a70d8dcbeb8feb
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-04-14 13:42:07 +0530

    buiding solidity smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+
+
+module.exports = {
+  solidity: "0.8.4",
+  // some network configurations
+  networks: {
+    hardhat: {
+      chainId: 1337 //local host of metmask have this chainID
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  }
+};

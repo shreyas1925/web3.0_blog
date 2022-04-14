5685bbb35d187f32081908e11fcc74da57ec99e6
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-04-14 16:13:09 +0530

    deploying the contract to local network

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+
+const fs = require('fs');
+
+async function main() {
+  /* these two lines deploy the contract to the network */
+  const Blog = await hre.ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My blog");
+
+  await blog.deployed();
+  console.log("Blog deployed to:", blog.address);
+
+  // It will create a filr named config and those details there which we will be using in our app
+  fs.writeFileSync('./config.js', `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- test/sample-test.js
+++ test/sample-test.js
@@ -34,3 +34,6 @@ describe("Blog", function () {
   })
 
 });
+
+// Account #0: 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 (10000 ETH)
+// Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
